fda2df30af53c3d17a85244dd0e7b647b94e560c
Nestor Mosquera <yunsde18@gmail.com>
2022-03-29 16:03:49 -0500

    new reset

--- src/controllers/menuController.js
+++ src/controllers/menuController.js
@@ -3,51 +3,45 @@ import Admin from "../models/adminModel.js";
 import Menu from "../models/menuModel.js";
 
 export const create = async (req, res) => {
-    
+  const { title, email, storeId } = req.body.props;
 
-    const {title, email, storeId} = req.body.props;
+  const newTitle = title;
+  const admin = await Admin.findOne({ email });
+  const id = admin.stores.findIndex((s) => s._id == storeId);
 
-    const newTitle = title;
-    const admin = await Admin.findOne({email});
-    const id = admin.stores.findIndex((s) => s._id == storeId);
-    
-    try{
-        if(admin){
-            console.log(admin.stores[id])
-            admin.stores[id].menus.unshift({
-                title: newTitle,
-            });
-        }
-    
-        await admin.save();
-        res.status(201).json(admin);
-
-    }catch(err){
-        res.status(404).json({message: err.message})
-
+  try {
+    if (admin) {
+      console.log(admin.stores[id]);
+      admin.stores[id].menus.unshift({
+        title: newTitle,
+      });
     }
 
+    await admin.save();
+    res.status(201).json(admin);
+  } catch (err) {
+    res.status(404).json({ message: err.message });
+  }
 };
-  
 
-export const getAll = async(req, res) => {
-    const {email, storeId} = req.query;
+export const getAll = async (req, res) => {
+  const { email, storeId } = req.query;
 
-    const admin = await Admin.findOne({email});
+  const admin = await Admin.findOne({ email });
 
-    if(admin)
-    {
-        const id = admin.stores.findIndex((s) => s._id == storeId);
-    }
+  try {
+    if (admin) {
+      const id = admin.stores.findIndex((s) => s._id == storeId);
 
-    const menus = await admin.stores[id].menus;
-
-    console.log(menus)
-    if(menus){
+      console.log(menus);
+      if (menus) {
         res.status(200).json(menus);
+      }
     }
-}
-
+  } catch (err) {
+    res.status(404).json({ message: err.message });
+  }
+};
 
 // export const delete = async (req, res) => {
 
@@ -56,7 +50,7 @@ export const getAll = async(req, res) => {
 //     const admin = await Admin.findOne({email:req.query.email})
 
 //     const storeId = admin.stores.findIndex((s) => s._id == req.query.storeId)
-    
+
 //     const coverId = admin.stores[storeId].bookings.findIndex((s) => s._id == req.params.id);
 
 //     const store = admin.stores[storeId].bookings.splice(coverId, 1);
@@ -68,29 +62,30 @@ export const getAll = async(req, res) => {
 //     });
 // }
 
-
 export const updateBooking = async (req, res) => {
-    console.log("entro")
-    console.log(req.body)
+  console.log("entro");
+  console.log(req.body);
 
-    const admin = await Admin.findOne({email:req.body.email})
-    
-    const storeId = admin.stores.findIndex((s) => s._id == req.body.storeId)
-    
-    const coverId = admin.stores[storeId].bookings.findIndex((s) => s._id == req.params.id);
+  const admin = await Admin.findOne({ email: req.body.email });
 
-    admin.stores[storeId].bookings[coverId] = {
-        info: req.body.info,
-        cupo: req.body.cupo,
-        date : req.body.date,
-        hour : req.body.hour,
-        description: req.body.description,
-        state: req.bodu.state,
-    };
+  const storeId = admin.stores.findIndex((s) => s._id == req.body.storeId);
 
-    await admin.save();
+  const coverId = admin.stores[storeId].bookings.findIndex(
+    (s) => s._id == req.params.id
+  );
 
-    res.json({
-        message: 'Product were deleted successfully'
-    });
-}
+  admin.stores[storeId].bookings[coverId] = {
+    info: req.body.info,
+    cupo: req.body.cupo,
+    date: req.body.date,
+    hour: req.body.hour,
+    description: req.body.description,
+    state: req.bodu.state,
+  };
+
+  await admin.save();
+
+  res.json({
+    message: "Product were deleted successfully",
+  });
+};
