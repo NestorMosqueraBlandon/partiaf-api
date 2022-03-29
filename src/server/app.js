1a149281e9427f89272e0cf5425ac1c5c5c49d6d
Nestor Mosquera <yunsde18@gmail.com>
2022-03-29 16:11:06 -0500

    new reset

--- src/server/app.js
+++ src/server/app.js
@@ -14,8 +14,8 @@ const app = express();
 app.set("port",  process.env.PORT || 4200);
 
 // middlewares
-app.use(express.json({limit: "30mb", extended:true}));
-app.use(express.urlencoded({ limit: "30mb", extended: false }));
+app.use(express.json());
+app.use(express.urlencoded({extended: false }));
 app.use(cors())
 app.use(morgan('dev'))
 
