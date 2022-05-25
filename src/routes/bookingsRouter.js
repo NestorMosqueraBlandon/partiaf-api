b1ce518f29fd594fdafce6ba2d7b9b5f8f504fe3
Nestor Mosquera <yunsde18@gmail.com>
2022-05-25 18:36:49 -0500

    update

--- src/routes/bookingsRouter.js
+++ src/routes/bookingsRouter.js
@@ -2,7 +2,7 @@ import { Router } from "express";
 import * as bookingsCtrl from '../controllers/bookingsController.js'
 const router = Router();
 
-router.put("/create", bookingsCtrl.create)
+router.put("/", bookingsCtrl.create)
 router.get("/", bookingsCtrl.getAll)
 // router.delete("/:id", buyCtrl.delete)
 // router.put("/:id", buyCtrl.update)
