11eac0b27d9e3fe65a20e577b88ca1204e15ed45
Nestor Mosquera <yunsde18@gmail.com>
2022-06-01 15:37:28 -0500

    update

--- src/routes/commentRouter.js
+++ src/routes/commentRouter.js
@@ -2,7 +2,7 @@ import { Router } from "express";
 import * as commentCtrl from '../controllers/commentController.js'
 const router = Router();
 
-router.post("/create", commentCtrl.create)
+router.put("/", commentCtrl.create)
 router.get("/", commentCtrl.getAll)
 router.delete("/:id", commentCtrl.deleteMenu)
 router.put("/:id", commentCtrl.updateMenu)
